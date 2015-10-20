1c9082bf12fedcc43a7c0250338949b5098cb851
James Peters <jpeters@teambuildr.com>
2015-10-19 21:27:17 -0400

    Inventory number format update
    Changed inventory quantity format to X,XXX (no decimal)

--- src/inventory.php
+++ src/inventory.php
@@ -10,7 +10,7 @@ foreach($data as $inv) {
 	if(substr($inv['ItemId'], 0, 3) == "NOU") {
 		$inventory['data'][] = array(
 			'ItemId' => $inv['ItemId'],
-			'AvailPhysical' => number_format($inv['AvailPhysical'],2,".",","),
+			'AvailPhysical' => number_format($inv['AvailPhysical'],0,".",","),
 			'BatchNumber' => (!empty($inv['BatchNumber']) ? $inv['BatchNumber'] : ""),
 			'Location' => (!empty($inv['Location']) ? $inv['Location'] : ""),
 			'expDate' => (!empty($inv['expDate']) ? date("m/d/y", strtotime($inv['expDate'])) : "N/A"),
