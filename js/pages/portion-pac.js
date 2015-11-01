f65604339546bbfd97f3828c464bc5f38a816a80
James Peters <jpeters@teambuildr.com>
2015-11-01 14:50:03 -0500

    Updated inventory functions

--- js/pages/portion-pac.js
+++ js/pages/portion-pac.js
@@ -6,7 +6,7 @@ function inventoryCheck(user_token) {
 		if(inventory.count > 0) {
 			$("#inventory_list").html('<table class="table sortable"><thead><tr><th class="width_180">Item #</th><th>Product Name</th><th class="text_right">Quantity</th><th>UOM</th><th>Case</th><th>Sell UOM</th><th>Pallet</th><th>Location</th></tr></thead><tbody></tbody></table>');
 			jQuery.each( inventory.data, function( i, inv ) {
-				$("#inventory_list tbody").append('<tr><td class="width_180">'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td>'+inv.BOMUnitId+'</td><td></td><td></td><td></td><td>'+inv.Location+'</td></tr>');
+				$("#inventory_list tbody").append('<tr><td class="width_180">'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td>'+inv.BOMUnitId+'</td><td>'+inv.Class+'</td><td>'+inv.SellUOM+'</td><td>'+inv.Pallet+'</td><td>'+inv.Location+'</td></tr>');
 			});
 			$.bootstrapSortable(false);
 		} else {
--- src/inventory.php
+++ src/inventory.php
@@ -1,27 +1,48 @@
 <?php
-$xml = simplexml_load_file("../Data/Onhand/onhandOverview.xml");
-$json = json_encode($xml);
-$array = json_decode($json,TRUE);
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+include("C:/inetpub/protected/database_connect.php");
 
-$data = $array['Body']['MessageParts']['MAX_InventOnhand']['MAX_InventOnhandTmp'];
+$user_token = $_GET[user_token];
 
-foreach($data as $inv) {
-	//Item # = NOU
-	if(substr($inv['ItemId'], 0, 3) == "NOU") {
-		$inventory['data'][] = array(
-			'ItemGroupId' => $inv['ItemGroupId'],
-			'ItemId' => $inv['ItemId'],
-			'AvailPhysical' => number_format($inv['AvailPhysical'],0,".",","),
-			'BatchNumber' => (!empty($inv['BatchNumber']) ? $inv['BatchNumber'] : ""),
-			'Location' => (!empty($inv['Location']) ? $inv['Location'] : ""),
-			'expDate' => (!empty($inv['expDate']) ? date("m/d/y", strtotime($inv['expDate'])) : "N/A"),
-			'ItemName' => (!empty($inv['ItemName']) ? $inv['ItemName'] : ""),
-			'ItemGroupId' => (!empty($inv['ItemGroupId']) ? $inv['ItemGroupId'] : ""),
-			'BOMUnitId' => (!empty($inv['BOMUnitId']) ? $inv['BOMUnitId'] : "")
-		);
+if(!empty($user_token)) {
+	$conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBAPP) or die('Could not select database.');
+	$result = mysqli_query($conn, "SELECT `company` FROM `users` WHERE `token`='$user_token'") or die(mysqli_error($conn));
+	if(mysqli_num_rows($result) > 0) {
+		$row = mysqli_fetch_array($result);
+		if($row['company'] == 4) {
+
+		} else {
+
+		}
+		$xml = simplexml_load_file("../Data/Onhand/onhandOverview.xml");
+		$json = json_encode($xml);
+		$array = json_decode($json,TRUE);
+
+		$data = $array['Body']['MessageParts']['MAX_InventOnhand']['MAX_InventOnhandTmp'];
+
+		foreach($data as $inv) {
+			//Item # = NOU
+			if(substr($inv['ItemId'], 0, 3) == "NOU") {
+				$inventory['data'][] = array(
+					'ItemGroupId' => $inv['ItemGroupId'],
+					'ItemId' => $inv['ItemId'],
+					'AvailPhysical' => number_format($inv['AvailPhysical'],0,".",","),
+					'BatchNumber' => (!empty($inv['BatchNumber']) ? $inv['BatchNumber'] : ""),
+					'Location' => (!empty($inv['Location']) ? $inv['Location'] : ""),
+					'expDate' => (!empty($inv['expDate']) ? date("m/d/y", strtotime($inv['expDate'])) : "N/A"),
+					'ItemName' => (!empty($inv['ItemName']) ? $inv['ItemName'] : ""),
+					'ItemGroupId' => (!empty($inv['ItemGroupId']) ? $inv['ItemGroupId'] : ""),
+					'BOMUnitId' => (!empty($inv['BOMUnitId']) ? $inv['BOMUnitId'] : ""),
+					'Case' => (!empty($inv['Case']) ? number_format(($inv['AvailPhysical']/$inv['Case']),0,".",",") : ""),
+					'SellUOM' => (!empty($inv['SellUOM']) ? $inv['SellUOM'] : ""),
+					'Pallet' => (!empty($inv['Pallet']) ? number_format(($inv['AvailPhysical']/$inv['Pallet']),0,".",",") : "")
+				);
+			}
+		}
+
+		$inventory['count'] = count($inventory['data']);
+	} else {
+		$inventory['count'] = 0;
 	}
-}
-
-$inventory['count'] = count($inventory['data']);
 
 echo json_encode($inventory);
