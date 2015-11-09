c40095c46781c0c8c2354103a18c4dcef0ed2bd2
James Peters <jpeters@teambuildr.com>
2015-11-09 09:02:46 -0500

    Updated aging function

--- src/cust_aging.php
+++ src/cust_aging.php
@@ -3,4 +3,4 @@ $xml = simplexml_load_file("../Data/CustAging/custAging.xml");
 $json = json_encode($xml);
 $array = json_decode($json,TRUE);
 
-echo print_r($array,true);
+echo '<pre>'.print_r($array,true);
