5e1f046ead74ca39b226d1ee026d500fe9407be3
James Peters <jpeters@teambuildr.com>
2015-11-17 10:46:33 -0500

    Updated Cust Aging Array Format

--- src/cust_aging.php
+++ src/cust_aging.php
@@ -9,12 +9,40 @@ $days_180 = strtotime("-180 days", $today);
 $days_90 = strtotime("-90 days", $today);
 $days_60 = strtotime("-60 days", $today);
 $days_30 = strtotime("-30 days", $today);
-$not_due = $due_now = $due_30 = $due_60 = $due_90 = $due_180 = 0;
+$cust_aging['data']['due']['180']['count'] = $cust_aging['data']['due']['90']['count'] = $cust_aging['data']['due']['60']['count'] = $cust_aging['data']['due']['30']['count'] = $cust_aging['data']['due']['now']['count'] = $cust_aging['data']['due']['not']['count'] = 0;
 
 foreach($data as $item) {
   $due_date = (!empty($item['DueDate']) ? $item['DueDate'] : "");
 
-  $cust_aging['data'][] = array (
+
+  if(!empty($due_date)) {
+    $cur_date = strtotime($due_date);
+
+    if($cur_date > $today) {  //If selected date is in the future
+      $cust_aging['data']['due']['not']['count']++;
+      $array_insert = "not";
+    } else if ($today >= $cur_date && $cur_date > $days_30) { //If selected date is within the last 30 days
+      $cust_aging['data']['due']['now']['count']++;
+      $array_insert = "now";
+    } else if ($days_30 >= $cur_date && $cur_date > $days_60) { //If selected date is within the last 60 days
+      $cust_aging['data']['due']['30']['count']++;
+      $array_insert = "30";
+    } else if ($days_60 >= $cur_date && $cur_date > $days_90) { //If selected date is within the last 90 days
+      $cust_aging['data']['due']['60']['count']++;
+      $array_insert = "60";
+    } else if ($days_90 >= $cur_date && $cur_date > $days_180) { //If selected date is within the last 180 days
+      $cust_aging['data']['due']['90']['count']++;
+      $array_insert = "90";
+    } else {
+      $cust_aging['data']['due']['180']['count']++;
+      $array_insert = "180";
+    }
+  } else {
+    $not_due++;
+    $array_insert = "not";
+  }
+
+  $cust_aging['data']['due'][$array_insert]['items'] = array (
     'AccountNum' => $item['AccountNum'],
     'AmountCur' => number_format($item['AmountCur'],2,".",","),
     'CompanyName' => $item['CompanyName'],
@@ -22,35 +50,6 @@ foreach($data as $item) {
     'DueDate' => $due_date,
     'InvoiceId' => (!empty($item['InvoiceId']) ? $item['InvoiceId'] : "")
   );
-
-  if(!empty($due_date)) {
-    $cur_date = strtotime($due_date);
-
-    if($cur_date > $today) {  //If selected date is in the future
-      $not_due++;
-    } else if ($today >= $cur_date && $cur_date > $days_30) { //If selected date is within the last 30 days
-      $due_now++;
-    } else if ($days_30 >= $cur_date && $cur_date > $days_60) { //If selected date is within the last 60 days
-      $due_30++;
-    } else if ($days_60 >= $cur_date && $cur_date > $days_90) { //If selected date is within the last 90 days
-      $due_60++;
-    } else if ($days_90 >= $cur_date && $cur_date > $days_180) { //If selected date is within the last 180 days
-      $due_90++;
-    } else {
-      $due_180++;
-    }
-  } else {
-    $not_due++;
-  }
 }
-
-$cust_aging['due'] = array(
-  '180' => $due_180,
-  '90' => $due_90,
-  '60' => $due_60,
-  '30' => $due_30,
-  'now' => $due_now,
-  'not' => $not_due
-);
 
 echo '<pre>'.print_r($cust_aging,true);
