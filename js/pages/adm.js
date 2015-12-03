bfcb873c8623a728dc6b947e5c0d83362a2150a9
Christopher Cole <christophercole1987@icloud.com>
2015-12-03 16:46:13 -0600

    More styling

--- js/pages/adm.js
+++ js/pages/adm.js
@@ -4,11 +4,11 @@ function inventoryTransactions(user_token) {
     $.get("/src/inv_trans.php?act=list&user_token="+user_token,function(result) {
         var inventory = jQuery.parseJSON(result);
         if(inventory.count > 0) {
-            $("#inv_trans_list").html('<table class="table sortable"><thead><!--<tr><th class="width_180">Company ID</th><th>Date Physical</th><th class="text_right">Invent Trans Ref Id</th><th>Invent Trans Type</th><th class="text_right">Invent Unit Id</th><th>Item Id</th><th class="text_right">Item Name</th><th>Qty</th></tr>--></thead><tbody></tbody><tfoot></foot></table>');
+            $("#inv_trans_list").html('');
             jQuery.each( inventory.data, function( t, inv ) {
-                $("#inv_trans_list tbody").append('<tr><td class="width_180" style="font-weight: bold">'+t+'</td></tr>');
+                $("#inv_trans_list tbody").append('<div class="table-responsive"><h3 style="border-bottom: 1px solid #bbb; padding-bottom: 10px;">'+t+'</h3></div>');
                 jQuery.each(inv, function (i, item) {
-                    $("#inv_trans_list tbody").append('<tr><td class="width_180" style="text-decoration: underline">'+i+'</td></tr>');
+                    $("#inv_trans_list tbody").append('<h4>'+i+'</h4><table class="table sortable"><thead><tr><th data-defaultsort="asc">Lot # / Reference</th><th>Qty</th><th>UOM</th></tr></thead><tbody></tbody><tfoot></tfoot></table>');
                     jQuery.each(item, function (j, jtem) {
                         $("#inv_trans_list tbody").append('<tr><td class="width_180">'+jtem.ReferenceId+'</td><td>'+jtem.Qty+'</td><td>'+jtem.UOM+'</td></tr>');
                     });
