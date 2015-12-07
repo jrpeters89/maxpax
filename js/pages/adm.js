8abd0e160420c0a0f901a7c3329d4f3cf3f1121b
Christopher Cole <christophercole1987@icloud.com>
2015-12-07 17:44:37 -0600

    Refresh data on date changes

--- js/pages/adm.js
+++ js/pages/adm.js
@@ -63,3 +63,38 @@ function inventoryTransactions(user_token) {
         }
     });
 }
+
+function refreshInvTransDates() {
+    var startDateTxt = document.getElementById("startDatePicker");
+    var endDateTxt = document.getElementById("endDatePicker");
+    startDateTxt.value = startDate;
+    endDateTxt.value = endDate;
+
+    $.get("/src/inv_trans.php?act=list&user_token=" + user_token + "&start_date=" + startDateTxt.value + "&end_date=" + endDateTxt.value, function (result) {
+        var inventory = jQuery.parseJSON(result);
+        if (inventory.count > 0) {
+            $("#inv_trans_list").html('');
+
+            //$("#inv_trans_list").append('<p>Start Date: <input type="text" id="startDatePicker"></p> ');
+            jQuery.each(inventory.data, function (t, inv) {
+                $("#inv_trans_list").append('<div id="inv_report' + t + '" class="table-responsive"><h3 style="border-bottom: 1px solid #bbb; padding-bottom: 10px;">' + t + '</h3></div>');
+                /*var cur_trans = 0;*/
+                jQuery.each(inv, function (i, item) {
+                    $("#inv_report" + t + "").append('<h4>' + i + '</h4><table id="inv_rpt_tbl_' + i + '" class="table sortable"><thead><tr><th data-defaultsort="asc">Lot # / Reference</th><th>Qty</th><th>UOM</th><th>Date</th></tr></thead><tbody></tbody><tfoot></tfoot></table>');
+                    jQuery.each(item, function (j, jtem) {
+                        $("#inv_rpt_tbl_" + i + " tbody").append('<tr><td class="width_180">' + jtem.Lot + ' / ' + jtem.ReferenceId + '</td><td>' + jtem.Qty + '</td><td>' + jtem.UOM + '</td><td>' + jtem.Date + '</td></tr>');
+                    });
+                });
+                /*cur_trans++;*/
+
+            });
+            /*if(typeof inventory.total != "undefined") {
+             $("#inv_trans_list tfoot").append('<tr><td colspan="2">TOTAL</td><td class="text_right">'+inventory.total.quantity+'</td><td></td><td class="text_right">'+inventory.total.case+'</td><td></td><td class="text_right">'+inventory.total.pallet+'</td></tr>');
+             }*/
+
+            $.bootstrapSortable(false);
+        } else {
+            $("#inv_trans_list").html("No Inventory Transactions Available");
+        }
+    });
+}
