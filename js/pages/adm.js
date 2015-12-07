6a98e555ecb25d5b63ea2ef1c87c20119e7214bc
Christopher Cole <christophercole1987@icloud.com>
2015-12-07 12:08:01 -0600

    Adding Datepicker function to index page to remove load errors

--- js/pages/adm.js
+++ js/pages/adm.js
@@ -9,7 +9,7 @@ function inventoryTransactions(user_token) {
             //$("#inv_trans_list").append('<script src="http://code.jquery.com/jquery-1.10.2.js"></script>');
             //$("#inv_trans_list").append('<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>');
             //$("#inv_trans_list").append('<script>$(function() { $("#datepicker").datepicker();});</script>');
-            $("#inv_trans_list").append('<p>Start Date: <input type="text" id="datepicker"></p> ');
+            $("#inv_trans_list").append('<p>Start Date: <input type="text" id="startDatePicker"></p> ');
             jQuery.each( inventory.data, function( t, inv ) {
                 $("#inv_trans_list").append('<div id="inv_report'+t+'" class="table-responsive"><h3 style="border-bottom: 1px solid #bbb; padding-bottom: 10px;">'+t+'</h3></div>');
                 /*var cur_trans = 0;*/
