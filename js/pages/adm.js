5a8f22f2eea98cc06854fa06be73a8e3a27f9203
Christopher Cole <christophercole1987@icloud.com>
2015-12-07 11:47:33 -0600

    Adding DatePicker to ADM portal

--- js/pages/adm.js
+++ js/pages/adm.js
@@ -5,7 +5,7 @@ function inventoryTransactions(user_token) {
         var inventory = jQuery.parseJSON(result);
         if(inventory.count > 0) {
             $("#inv_trans_list").html('');
-            $("#inv_trans_list").append('<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">');
+            $("#inv_trans_list").append('<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">');
             $("#inv_trans_list").append('<script src="http://code.jquery.com/jquery-1.10.2.js"></script>');
             $("#inv_trans_list").append('<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>');
             $("#inv_trans_list").append('<script>$(function() { $("#datepicker").datepicker();});</script>');
