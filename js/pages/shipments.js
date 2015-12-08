6ae4eef66daf9ed74979a4f6cbfc4144fdb6b95b
Christopher Cole <christophercole1987@icloud.com>
2015-12-08 09:28:42 -0600

    Working on data for GoPicnic Shipment

--- js/pages/shipments.js
+++ js/pages/shipments.js
@@ -7,7 +7,7 @@ function shippingData(user_token) {
             $("#shipments_list").html('<table class="table sortable"><thead><tr><th>Packing Slip #</tr></thead><tbody></tbody><tfoot></foot></table>');
             //$("#shipments_list").html('<table class="table sortable"><thead><tr><th class="text_right">Packing Slip #</th><th>Ship Date</th><th>Item #</th><th>Description</th><th class="text_right">Sales Order #</th><th class="text_right">Customer Ref</th></tr></thead><tbody></tbody><tfoot></foot></table>');
             jQuery.each(shipments.data, function (x, psId) {
-                $("#shipments_list tbody").append('<tr><td>' + psId.PackingSlipId + '</td> <td>' + psId.Item + '</td></tr>');
+                $("#shipments_list tbody").append('<tr><td>' + psId + '</td> <td>' + psId.Item + '</td></tr>');
                 jQuery.each(psId, function (z, det) {
                     $("#shipments_list tbody").append('<tr><td>' + det.ShipDate + '</td><td class="text_right">' + det.Item + '</td><td class="text_right">' + det.Description + '</td><td>' + det.SalesOrder + '</td><td class="text_right">' + det.CustomerRef + '</td></tr>');
                 });
