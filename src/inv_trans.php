f2d66e9be80171d53240241b531b2e1f2b351c94
Christopher Cole <christophercole1987@icloud.com>
2015-12-08 12:51:15 -0600

    Trying to find where duplicates are happening

--- src/inv_trans.php
+++ src/inv_trans.php
@@ -22,7 +22,7 @@ if (!empty($user_token)) {
 
         foreach ($data as $item) {
             if ((substr($item['ItemId'], 0, 3) == "ADM") && ($item['CompanyId'] === "usp") && ($item['Qty'] != null) && ($item['InventTransRefId'] != null) && ($item['InventBatchId'] != null) && ($item['DatePhysical'] != null)  &&  ($item['DatePhysical'] > $start_date) && ($item['DatePhysical'] < $end_date)) {
-                    $inv_trans['data'][$item['ItemId']][$item['InventTransType']][$item['InventBatchId']][] = array(
+                    $inv_trans['data'][$item['ItemId']][$item['InventTransType']][] = array(
                         'ReferenceId' => $item['InventTransRefId'],
                         'Qty' => number_format($item['Qty'], 2, ".", ","),
                         'UOM' => $item['InventUnitId'],
