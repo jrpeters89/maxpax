b89947e3f724080454446cf25ac00d1cee86a0cb
Christopher Cole <christophercole1987@icloud.com>
2015-12-08 11:43:30 -0600

    Trying to remove duplicates

--- js/pages/adm.js
+++ js/pages/adm.js
@@ -44,7 +44,7 @@ function inventoryTransactions(user_token) {
                 jQuery.each(inv, function (i, item) {
                     $("#inv_report" + t + "").append('<h4>' + i + '</h4><table id="inv_rpt_tbl_' + i + '" class="table sortable"><thead><tr><th data-defaultsort="asc">Lot # / Reference</th><th>Qty</th><th>UOM</th><th>Date</th></tr></thead><tbody></tbody><tfoot></tfoot></table>');
                     jQuery.each(item, function (test, test1){
-                    jQuery.each(item, function (j, jtem) {
+                    jQuery.each(test1, function (j, jtem) {
                         $("#inv_rpt_tbl_" + i + " tbody").append('<tr><td class="width_180">' + jtem.Lot + ' / ' + jtem.ReferenceId + '</td><td>' + jtem.Qty + '</td><td>' + jtem.UOM + '</td><td>' + jtem.Date + '</td></tr>');
                     });
                     });
