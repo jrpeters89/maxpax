b91ca4ae362287d5c233380de1f1a602347db29a
Christopher Cole <christophercole1987@icloud.com>
2015-12-08 12:47:20 -0600

    Trying to find where duplicates are happening

--- src/inv_trans.php
+++ src/inv_trans.php
@@ -18,19 +18,22 @@ if (!empty($user_token)) {
 
         $data = $array['Body']['MessageParts']['InventTransAPP']['InventTrans'];
 
+        $i = 0;
+
         foreach ($data as $item) {
             if ((substr($item['ItemId'], 0, 3) == "ADM") && ($item['CompanyId'] === "usp") && ($item['Qty'] != null) && ($item['InventTransRefId'] != null) && ($item['InventBatchId'] != null) && ($item['DatePhysical'] != null)  &&  ($item['DatePhysical'] > $start_date) && ($item['DatePhysical'] < $end_date)) {
                     $inv_trans['data'][$item['ItemId']][$item['InventTransType']][$item['InventBatchId']][] = array(
                         'ReferenceId' => $item['InventTransRefId'],
                         'Qty' => number_format($item['Qty'], 2, ".", ","),
                         'UOM' => $item['InventUnitId'],
-                        'Lot' => $item['InventBatchId'],
+                        'Lot' => $i + ". " + $item['InventBatchId'],
                         'Date' => $item['DatePhysical'],
                         'ItemId' => $item['ItemId'],
                         'TransType' => $item['InventTransType']
                     );
 
             }
+            $i++;
         }
 
         $inv_trans['count'] = count($inv_trans['data']);
