4eb306a1e1eb614b633d2b4dd15db55a5354938d
Christopher Cole <christophercole1987@icloud.com>
2015-12-08 13:07:17 -0600

    Trying to find where duplicates are happening

--- js/pages/adm.js
+++ js/pages/adm.js
@@ -82,7 +82,7 @@ function refreshInvTransDates(user_token) {
                 jQuery.each(inv, function (i, item) {
                     $("#inv_report" + t + "").append('<h4>' + i + '</h4><table id="inv_rpt_tbl_' + i + '" class="table sortable"><thead><tr><th data-defaultsort="asc">Lot # / Reference</th><th>Qty</th><th>UOM</th><th>Date</th></tr></thead><tbody></tbody><tfoot></tfoot></table>');
                     jQuery.each(item, function (j, jtem) {
-                        $("#inv_rpt_tbl_" + i + " tbody").append('<tr><td class="width_180">' + jtem.Lot + ' / ' + jtem.ReferenceId + '</td><td>' + jtem.Qty + '</td><td>' + jtem.UOM + '</td><td>' + jtem.Date + '</td></tr>');
+                        $("#inv_rpt_tbl_" + i + " tbody").append('<tr><td class="width_180">' + j + ' / ' + jtem.Lot + ' / ' + jtem.ReferenceId + '</td><td>' + jtem.Qty + '</td><td>' + jtem.UOM + '</td><td>' + jtem.Date + '</td></tr>');
                     });
 
                 });
