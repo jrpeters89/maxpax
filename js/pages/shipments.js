1d0560611420ca30f15b30624f543e239922fb81
Christopher Cole <christophercole1987@icloud.com>
2015-12-09 12:58:21 -0600

    Formatting Data for GoPicnic

--- js/pages/shipments.js
+++ js/pages/shipments.js
@@ -12,7 +12,7 @@ function shippingData(user_token) {
                 $("slip_id_table_" + psId.PackingSlipId).append('<tr><td>' + x + '</td><td>' + psId.ShipDate + '</td> <td>' + psId.Item + '</td><td>' + psId.Description + '</td><td>' + psId.SalesOrder + '</td><td>' + psId.CustomerRef + '</td></tr>');
                 $("#shipments_list").append('<div id="slip_id_' + psId.PackingSlipId + '_lot" class="table-responsive"><table id="slip_id_table_' + psId.PackingSlipId +'_lot" class="table_sortable"><th>Lot #</th><th>Exp. Date</th><th>Delivered</th><th>UOM</th></table> </div>');
                 jQuery.each(psId, function (z, det) {
-                    $("#slip_id_" + psId.PackingSlipId + "_lot").append('<tr><td>' + det.Lot + '</td><td class="text_right">' + det.ExpirationDate + '</td><td class="text_right">' + det.Delivered + '</td><td>' + det.UOM + '</td></tr>');
+                    $("#slip_id_" + psId.PackingSlipId + "_lot tbody").append('<tr><td>' + det.Lot + '</td><td class="text_right">' + det.ExpirationDate + '</td><td class="text_right">' + det.Delivered + '</td><td>' + det.UOM + '</td></tr>');
                 });
             });
             // if(typeof shipments.total != "undefined") {
