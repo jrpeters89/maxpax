df32219db9bd8fc6c4e39d80ce34cf8ef496ee0f
Christopher Cole <christophercole1987@icloud.com>
2015-12-11 12:15:20 -0600

    Searching for file that starts with packing slip

---
+++ src/shipments_file.php
@@ -0,0 +1,34 @@
+<?php
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+include("C:/inetpub/protected/database_connect.php");
+
+$user_token = $_GET[user_token];
+
+if(!empty($user_token)) {
+    $conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBAPP) or die('Could not select database.');
+    $result = mysqli_query($conn, "SELECT `access_level`,`documents` FROM `users` LEFT JOIN `companies` ON `users`.`company` = `companies`.`id` WHERE `token`='$user_token'") or die(mysqli_error($conn));
+    if(mysqli_num_rows($result) > 0) {
+        $file = $_GET["loc"];
+        if(!empty($file)) {
+            //if (file_exists($file)) {
+            //if(glob($file.'-*.pdf')){
+            foreach(glob($file.'-*.pdf') as $filename) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment; filename='.basename($filename));
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($filename));
+                readfile($filename);
+                exit;
+            }
+        } else {
+            echo "Invalid file path";
+        }
+    } else {
+        echo "You do not have permission to view this file.";
+    }
+} else {
+    echo "You do not have permission to view this file.";
+}
