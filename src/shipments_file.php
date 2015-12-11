7fcd27fd57db23a3bd9bd431e679163c6a78b0ec
Christopher Cole <christophercole1987@icloud.com>
2015-12-11 12:48:25 -0600

    Searching for file that starts with packing slip

--- src/shipments_file.php
+++ src/shipments_file.php
@@ -12,7 +12,7 @@ if(!empty($user_token)) {
         if(!empty($file)) {
             //if (file_exists($file)) {
             //if(glob($file.'-*.pdf')){
-            $filename = glob($file . "*.pdf");
+            $filename = glob($file . "?*.pdf");
             echo('<script>alert("'.$file.'");</script>');
             echo('<script>alert("'.$filename[0].'");</script>');
             //foreach(glob($file."*.pdf") as $filename) {
