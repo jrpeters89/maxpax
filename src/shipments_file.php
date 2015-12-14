99d534b066138bd4c4beb3576300babe5cd6956e
Christopher Cole <christophercole1987@icloud.com>
2015-12-14 13:20:07 -0600

    working on pdf damage error

--- src/shipments_file.php
+++ src/shipments_file.php
@@ -4,29 +4,27 @@ include("C:/inetpub/protected/database_connect.php");
 
 $user_token = $_GET[user_token];
 
-if(!empty($user_token)) {
+if (!empty($user_token)) {
     $conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBAPP) or die('Could not select database.');
     $result = mysqli_query($conn, "SELECT `access_level`,`documents` FROM `users` LEFT JOIN `companies` ON `users`.`company` = `companies`.`id` WHERE `token`='$user_token'") or die(mysqli_error($conn));
-    if(mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) > 0) {
         $file = $_GET["loc"];
-        if(!empty($file)) {
+        if (!empty($file)) {
             $filename = glob($file . '*');
+            //foreach ($filename as $f) {
+                if (file_exists($filename[0])) {
+                    header('Content-Description: File Transfer');
+                    header('Content-Type: application/octet-stream');
+                    header('Content-Disposition: attachment; filename=' . basename($f));
+                    header('Expires: 0');
+                    header('Cache-Control: must-revalidate');
+                    header('Pragma: public');
+                    header('Content-Length: ' . filesize($f));
+                    readfile($f);
 
-            foreach($filename as $f) {
-            if(file_exists($f) ){
-                echo('<script>alert("'.$f.'");</script>');
-                header('Content-Description: File Transfer');
-                header('Content-Type: application/octet-stream');
-                header('Content-Disposition: attachment; filename='.basename($f));
-                header('Expires: 0');
-                header('Cache-Control: must-revalidate');
-                header('Pragma: public');
-                header('Content-Length: ' . filesize($f));
-                readfile($f);
-
-                exit;
-            }
-            }
+                    exit;
+                }
+            //}
         } else {
             echo "Invalid file path";
         }
