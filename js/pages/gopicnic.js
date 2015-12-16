b6fc33a6efa5f50f6b1a11c62d79f2d202efbe3a
Christopher Cole <christophercole1987@icloud.com>
2015-12-16 13:47:21 -0600

    Styling for subtotatl

--- js/pages/gopicnic.js
+++ js/pages/gopicnic.js
@@ -69,7 +69,7 @@ function shippingData(user_token) {
 					shipSubtotal += parseInt(det.Delivered, 10);
 					}
 				});
-				$("#slip_id_" + psId.PackingSlipId + "_lot tfoot").append('<tr><td colspan="2">Subtotal</td><td>' + shipSubtotal + '</td> </tr>');
+				$("#slip_id_" + psId.PackingSlipId + "_lot tfoot").append('<tr><td colspan="2" style="text-align: right">Subtotal</td><td>' + shipSubtotal + '</td> </tr>');
 			});
 
 			// if(typeof shipments.total != "undefined") {
@@ -92,14 +92,17 @@ function refreshShipmentDates(user_token) {
 		if (shipments.count > 0) {
 			$("#shipments_list").html('');
 			jQuery.each(shipments.data, function (x, psId) {
+				var shipSubtotal = 0;
 				$("#shipments_list").append('<div id="slip_id_' + psId.PackingSlipId + '" class="table-responsive"><table id="slip_id_table_' + psId.PackingSlipId +'" class="table sortable"><thead><tr><th class="width_180">Packing Slip #</th><th>Ship Date</th><th>Item #</th><th>Description</th><th>Sale Order #</th><th>Customer Ref</th></tr></thead><tbody><tr><td class="width_180"><a href="/src/shipments_file.php?loc=//safewayfs02/Shared/Docs-USP/SO/' + x +'&user_token=' + user_token +'">' + x + '</a></td><td>' + psId.ShipDate + '</td> <td>' + psId.Item + '</td><td>' + psId.Description + '</td><td>' + psId.SalesOrder + '</td><td>' + psId.CustomerRef + '</td></tr></tbody><tfoot></tfoot></table>');
 				//$("slip_id_table_" + psId.PackingSlipId + "tbody").append('<tr><td>' + x + '</td><td>' + psId.ShipDate + '</td> <td>' + psId.Item + '</td><td>' + psId.Description + '</td><td>' + psId.SalesOrder + '</td><td>' + psId.CustomerRef + '</td></tr>');
 				$("#shipments_list").append('<div id="slip_id_' + psId.PackingSlipId + '_lot" class="table-responsive"><table id="slip_id_table_' + psId.PackingSlipId +'_lot" class="table sortable"><thead><tr><th>Lot #</th><th>Exp. Date</th><th>Delivered</th><th>UOM</th></tr></thead><tbody></tbody><tfoot></tfoot></table> </div>');
 				jQuery.each(psId, function (z, det) {
 					if(typeof det.Lot != "undefined") {
 						$("#slip_id_" + psId.PackingSlipId + "_lot tbody").append('<tr><td>' + det.Lot + '</td><td>' + det.ExpirationDate + '</td><td>' + det.Delivered + '</td><td>' + det.UOM + '</td></tr>');
+						shipSubtotal += parseInt(det.Delivered, 10);
 					}
 				});
+				$("#slip_id_" + psId.PackingSlipId + "_lot tfoot").append('<tr><td colspan="2" style="text-align: right">Subtotal</td><td>' + shipSubtotal + '</td> </tr>');
 			});
 			// if(typeof shipments.total != "undefined") {
 			// 	$("#inventory_list tfoot").append('<tr><td colspan="2">TOTAL</td><td class="text_right">'+inventory.total.quantity+'</td><td></td><td class="text_right">'+inventory.total.case+'</td><td></td><td class="text_right">'+inventory.total.pallet+'</td></tr>');
