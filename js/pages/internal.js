9cd277a1633c1d700b9b6fd882d8b110e8c0d633
Christopher Cole <christophercole1987@icloud.com>
2016-01-08 12:44:18 -0600

    Testing switching companies on select

--- js/pages/internal.js
+++ js/pages/internal.js
@@ -5,6 +5,7 @@ $("body").on("click", "#main_menu li ul li a", function (event) {
 });
 
 function updateInternal(user_token) {
-
+    console.log("Internal Button Clicked");
     companyInfo(user_token, 1);
+
 }
