ca5adfd4342ac97c9c85c7c0314a02f01dd6e786
Christopher Cole <christophercole1987@icloud.com>
2016-01-08 12:20:10 -0600

    Testing switching companies on select

--- js/pages/internal.js
+++ js/pages/internal.js
@@ -1,12 +1,5 @@
 $("body").on("click", "#main_menu li ul li a", function (event) {
 
     companyInfo(' ', 1);
-    $("sales_link").addClass("menu_99");
-    appSetup(99,function () {
-        console.log("Page Changed");
-        $.getScript("/js/pages/documents.js", function () {
-            console.log("Documents Script Loaded");
-            changePage(company.default_page);
-        });
-    });
+
 });
