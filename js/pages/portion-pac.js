a80bc13b690ac31138ce71a8ce4e3fe919611c4b
Christopher Cole <christophercole1987@icloud.com>
2016-02-05 13:54:50 -0600

    Adding Production Date to PortionPac

--- js/pages/portion-pac.js
+++ js/pages/portion-pac.js
@@ -4,7 +4,7 @@ function inventoryCheck(user_token, company_id) {
 	$.get("/src/inventory.php?act=list&user_token="+user_token+"&company_id="+company_id,function(result) {
 		var inventory = jQuery.parseJSON(result);
 		if(inventory.count > 0) {
-			$("#inventory_list").html('<table class="table sortable"><thead><tr><th class="width_180">Item #</th><th>Product Name</th><th class="text_right">Quantity</th><th>UOM</th><th class="text_right">Case</th><th>Sell UOM</th><th class="text_right">Pallet</th><th>Prod Date</th></tr></thead><tbody></tbody><tfoot></foot></table>');
+			$("#inventory_list").html('<table class="table sortable"><thead><tr><th class="width_180">Item #</th><th>Product Name</th><th class="text_right">Quantity</th><th>UOM</th><th class="text_right">Case</th><th>Sell UOM</th><th class="text_right">Pallet</th><th class="width_180">Prod Date</th></tr></thead><tbody></tbody><tfoot></foot></table>');
 			jQuery.each( inventory.data, function( i, inv ) {
 				$("#inventory_list tbody").append('<tr><td class="width_180">'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td>'+inv.BOMUnitId+'</td><td class="text_right">'+inv.Case+'</td><td>'+inv.SellUOM+'</td><td class="text_right">'+inv.Pallet+'</td><td>'+inv.ProdDate+'</td></tr>');
 			});
