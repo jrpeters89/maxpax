2c04f488d53d6a685dd8ef4bc6c3e2523a96ec17
Christopher Cole <christophercole1987@icloud.com>
2016-02-23 13:26:16 -0600

    Add Shipments data to tab in Nourish Snacks

--- js/pages/nourish.js
+++ js/pages/nourish.js
@@ -69,3 +69,88 @@ function opensalesCheck(user_token,type) {
 		}
 	});
 }
+
+function shippingData(user_token) {
+	$("#shipments_list").html('<div id="loading"><img src="images/spin.gif" /></div>');
+	$("#shipments_container").show();
+	var today = new Date();
+	var dd = 1;
+	var mm = today.getMonth() + 1;
+	var yyyy = today.getFullYear();
+	if (dd < 10) {
+		dd = '0' + dd;
+	}
+	if (mm < 10) {
+		mm = '0' + mm;
+	}
+
+	var startDate = yyyy + '-' + mm + '-' + dd;
+	var endDate;
+	if (mm == 12) {
+		endDate = (yyyy + 1) + '-01-' + dd;
+	} else {
+		mm = mm + 1;
+		if (mm < 10) {
+			mm = '0' + mm;
+		}
+		endDate = yyyy + '-' + mm + '-' + dd;
+	}
+	//var endDate  = yyyy + '-' + (mm + 1) +'-' + dd;
+	var startDateTxt = document.getElementById("shipStartDatePicker");
+	var endDateTxt = document.getElementById("shipEndDatePicker");
+	startDateTxt.value = startDate;
+	endDateTxt.value = endDate;
+	$('#shipStartDatePicker').datepicker({
+		dateFormat: 'yy-mm-dd'
+	});
+	$('#shipEndDatePicker').datepicker({
+		dateFormat: 'yy-mm-dd'
+	});
+	$.get("/src/shipments.php?act=list&user_token=" + user_token + "&start_date=" + startDateTxt.value + "&end_date=" + endDateTxt.value, function (result) {
+		var shipments = jQuery.parseJSON(result);
+		if (shipments.count > 0) {
+			$("#shipments_list").html('');
+			jQuery.each(shipments.data, function (x, psId) {
+				$("#shipments_list").append('<div id="slip_id_' + psId.PackingSlipId + '" class="table-responsive"><table id="slip_id_table_' + psId.PackingSlipId +'" class="table sortable"><thead><tr><th class="width_180">Packing Slip #</th><th class="width_180">Ship Date</th><th class="width_180">Sale Order #</th><th class="width_180">Customer Ref</th></tr></thead><tbody><tr><td class="width_180"><a href="/src/shipments_file.php?loc=//safewayfs02/Shared/Docs-USP/SO/' + x +'&user_token=' + user_token +'">' + x + '</a></td><td>' + psId.ShipDate + '</td> <td>' + psId.SalesOrder + '</td><td>' + psId.CustomerRef + '</td></tr></tbody><tfoot></tfoot></table>');
+				$("#shipments_list").append('<div id="slip_id_' + psId.PackingSlipId + '_lot" class="table-responsive"><table id="slip_id_table_' + psId.PackingSlipId +'_lot" class="table sortable"><thead><tr><th class="width_180">Item # / Lot #</th><th class="width_180"> Description / Exp. Date</th><th class="width_180">Delivered</th><th class="width_180">UOM</th></tr><tr><td><strong>' + psId.Item + '</strong></td><td><strong>' + psId.Description + '</strong></td></tr></thead><tbody></tbody><tfoot></tfoot></table> </div>');
+				jQuery.each(psId, function (z, det) {
+					if(typeof det.Lot != "undefined") {
+						$("#slip_id_" + psId.PackingSlipId + "_lot tbody").append('<tr><td>' + det.Lot + '</td><td>' + det.ExpirationDate + '</td><td>' + det.Delivered + '</td><td>' + det.UOM + '</td></tr>');
+					}
+				});
+				$("#slip_id_" + psId.PackingSlipId + "_lot tfoot").append('<tr><td></td><td><strong>Subtotal</strong></td><td><strong>' + psId.Subtotal + '</strong></td><td></td></tr>');
+
+			});
+			$.bootstrapSortable(false);
+		} else {
+			$("#shipments_list").html("No Shipments Available");
+		}
+	});
+}
+function refreshShipmentDates(user_token) {
+	$("#shipments_list").html('');
+	$("#shipments_list").html('<div id="loading"><img src="images/spin.gif" /></div>');
+	$("#shipments_container").show();
+	var startDateTxt = document.getElementById("shipStartDatePicker");
+	var endDateTxt = document.getElementById("shipEndDatePicker");
+	$.get("/src/shipments.php?act=list&user_token=" + user_token + "&start_date=" + startDateTxt.value + "&end_date=" + endDateTxt.value, function (result) {
+		var shipments = jQuery.parseJSON(result);
+		if (shipments.count > 0) {
+			$("#shipments_list").html('');
+			jQuery.each(shipments.data, function (x, psId) {
+				$("#shipments_list").append('<div id="slip_id_' + psId.PackingSlipId + '" class="table-responsive"><table id="slip_id_table_' + psId.PackingSlipId +'" class="table sortable"><thead><tr><th class="width_180">Packing Slip #</th><th class="width_180">Ship Date</th><th class="width_180">Sale Order #</th><th class="width_180">Customer Ref</th></tr></thead><tbody><tr><td class="width_180"><a href="/src/shipments_file.php?loc=//safewayfs02/Shared/Docs-USP/SO/' + x +'&user_token=' + user_token +'">' + x + '</a></td><td>' + psId.ShipDate + '</td> <td>' + psId.SalesOrder + '</td><td>' + psId.CustomerRef + '</td></tr></tbody><tfoot></tfoot></table>');
+				$("#shipments_list").append('<div id="slip_id_' + psId.PackingSlipId + '_lot" class="table-responsive"><table id="slip_id_table_' + psId.PackingSlipId +'_lot" class="table sortable"><thead><tr><th class="width_180">Item # / Lot #</th><th class="width_180"> Description / Exp. Date</th><th class="width_180">Delivered</th><th class="width_180">UOM</th></tr><tr><td><strong>' + psId.Item + '</strong></td><td><strong>' + psId.Description + '</strong></td></tr></thead><tbody></tbody><tfoot></tfoot></table> </div>');
+				jQuery.each(psId, function (z, det) {
+					if(typeof det.Lot != "undefined") {
+						$("#slip_id_" + psId.PackingSlipId + "_lot tbody").append('<tr><td>' + det.Lot + '</td><td>' + det.ExpirationDate + '</td><td>' + det.Delivered + '</td><td>' + det.UOM + '</td></tr>');
+					}
+				});
+				$("#slip_id_" + psId.PackingSlipId + "_lot tfoot").append('<tr><td></td><td><strong>Subtotal</strong></td><td><strong>' + psId.Subtotal + '</strong></td><td></td></tr>');
+
+			});
+			$.bootstrapSortable(false);
+		} else {
+			$("#shipments_list").html("No Shipments Available");
+		}
+	});
+}
