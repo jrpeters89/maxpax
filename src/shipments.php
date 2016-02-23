974874f9faf49c34e4dd413ebc8f137b7dc0791e
Christopher Cole <christophercole1987@icloud.com>
2016-02-23 13:20:03 -0600

    Add Shipments data to tab in Nourish Snacks

--- src/shipments.php
+++ src/shipments.php
@@ -38,6 +38,28 @@ if (!empty($user_token)) {
                     $shipments['data'][$item['PackingSlipId']]['Subtotal'] += intval($item['Quantity']);
                 }
             }
+        } elseif ($row['company' == 3) { //Nourish Snacks
+            foreach ($data as $item) {
+                            if (($item['CompanyName'] === "US Packaging LLC") && ($item['CustomerNumber'] === "USP-C000041") && ($item['ShipDate'] >= $start_date) && ($item['ShipDate'] <= $end_date)) {
+                                $shipments['data'][$item['PackingSlipId']]['PackingSlipId'] = ($item['PackingSlipId'] != null ? $item['PackingSlipId'] : "");
+                                $shipments['data'][$item['PackingSlipId']]['ShipDate'] = ($item['ShipDate'] != null ? $item['ShipDate'] : "");
+                                $shipments['data'][$item['PackingSlipId']]['Item'] = ($item['Item'] != null ? $item['Item'] : "");
+                                $shipments['data'][$item['PackingSlipId']]['Description'] = ($item['Description'] != null ? $item['Description'] : "");
+                                $shipments['data'][$item['PackingSlipId']]['SalesOrder'] = ($item['SalesOrder'] != null ? $item['SalesOrder'] : "");
+                                $shipments['data'][$item['PackingSlipId']]['CustomerRef'] = ($item['CustomerRef'] != null ? $item['CustomerRef'] : "");
+
+                                $shipments['data'][$item['PackingSlipId']][] = array(
+                                    'Lot' => ($item['BatchNumber'] != null ? $item['BatchNumber'] : ""),
+                                    'ExpirationDate' => ($item['ExpirationDate'] != null ? $item['ExpirationDate'] : ""),
+                                    'Delivered' => ($item['Quantity'] != null ? intval($item['Quantity']) : ""),
+                                    'UOM' => ($item['Unit'] != null ? $item['Unit'] : "")
+                                );
+
+                                $shipments['data'][$item['PackingSlipId']]['Subtotal'] += intval($item['Quantity']);
+
+                            }
+                        }
+
         } else { //AMD filter
             foreach ($data as $item) {
                 if (($item['CompanyName'] === "US Packaging LLC") && ($item['CustomerNumber'] === "USP-C000030") && ($item['ShipDate'] >= $start_date) && ($item['ShipDate'] <= $end_date)) {
