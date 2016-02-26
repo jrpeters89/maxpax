36ab7c6324fcf7568abf5b5a91efe4f05fbd822d
Christopher Cole <christophercole1987@icloud.com>
2016-02-26 15:30:10 -0600

    Adding more ItemGroupId filters to portion pac inventory

--- src/inventory.php
+++ src/inventory.php
@@ -79,7 +79,39 @@ if(!empty($user_token)) {
 					$case = (!empty($inv['Case']) ? ($inv['AvailPhysical'] / $inv['Case']) : 0);
 					$pallet_num = (!empty($inv['Pallet']) ? $inv['Pallet'] : 0);
 					$pallet = (!empty($inv['Pallet']) ? ($inv['AvailPhysical'] / $inv['Pallet']) : 0);
-					if ((substr($inv['ItemGroupId'], 0, 2) == "FG" || substr($inv['ItemGroupId'], 0, 2) == "PM" || substr($inv['ItemGroupId'], 0, 2) == "LB") && substr($inv['ItemId'], 0, 3) == "PAC" && $inv['Location'] != "QUARANTINE" && $inv['CompanyName'] == "MaxPax LLC") {
+					if ((substr($inv['ItemGroupId'], 0, 2) == "FG" ) && substr($inv['ItemId'], 0, 3) == "PAC" && $inv['Location'] != "QUARANTINE" && $inv['CompanyName'] == "MaxPax LLC") {
+						$qty_total = $qty_total + $quantity;
+						$case_total = $case_total + $case;
+						$pallet_total = $pallet_total + $pallet;
+						if (!isset($items[$inv['ItemId']])) {
+							$inventory['data'][$i] = array(
+								'ItemId' => $inv['ItemId'],
+								'AvailPhysical' => number_format($quantity, 0, ".", ","),
+								'ItemName' => (!empty($inv['ItemName']) ? $inv['ItemName'] : ""),
+								'BOMUnitId' => (!empty($inv['BOMUnitId']) ? $inv['BOMUnitId'] : ""),
+								'CaseNum' => $case_num,
+								'Case' => number_format($case, 2, ".", ","),
+								'SellUOM' => (!empty($inv['SellUOM']) ? $inv['SellUOM'] : ""),
+								'PalletNum' => $pallet_num,
+								'Pallet' => number_format($pallet, 2, ".", ","),
+								'Location' => (!empty($inv['Location']) ? $inv['Location'] : ""),
+								'ProdDate' => (!empty($inv['prodDate']) ? $inv['prodDate'] : "")
+							);
+							$items[$inv['ItemId']] = $i;
+							$i++;
+						} else {
+							$inventory['data'][$items[$inv['ItemId']]]['AvailPhysical'] = number_format((str_replace(",", "", $inventory['data'][$items[$inv['ItemId']]]['AvailPhysical']) + str_replace(",", "", $quantity)), 0, ".", ",");
+							$inventory['data'][$items[$inv['ItemId']]]['Case'] = (!empty($inventory['data'][$items[$inv['ItemId']]]['CaseNum']) ? number_format((str_replace(",", "", $inventory['data'][$items[$inv['ItemId']]]['AvailPhysical']) / $inventory['data'][$items[$inv['ItemId']]]['CaseNum']), 2, ".", ",") : 0);
+							$inventory['data'][$items[$inv['ItemId']]]['Pallet'] = (!empty($inventory['data'][$items[$inv['ItemId']]]['PalletNum']) ? number_format((str_replace(",", "", $inventory['data'][$items[$inv['ItemId']]]['AvailPhysical']) / $inventory['data'][$items[$inv['ItemId']]]['PalletNum']), 2, ".", ",") : 0);
+						}
+					}
+				} else {
+					$quantity = (!empty($inv['AvailPhysical']) ? $inv['AvailPhysical'] : 0);
+					$case_num = (!empty($inv['Case']) ? $inv['Case'] : 0);
+					$case = (!empty($inv['Case']) ? ($inv['AvailPhysical'] / $inv['Case']) : 0);
+					$pallet_num = (!empty($inv['Pallet']) ? $inv['Pallet'] : 0);
+					$pallet = (!empty($inv['Pallet']) ? ($inv['AvailPhysical'] / $inv['Pallet']) : 0);
+					if ((substr($inv['ItemGroupId'], 0, 2) == "PM" || substr($inv['ItemGroupId'], 0, 2) == "LB") && substr($inv['ItemId'], 0, 3) == "PAC" && $inv['Location'] != "QUARANTINE" && $inv['CompanyName'] == "MaxPax LLC") {
 						$qty_total = $qty_total + $quantity;
 						$case_total = $case_total + $case;
 						$pallet_total = $pallet_total + $pallet;
