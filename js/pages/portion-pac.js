4e4adf8c65609a823e0b0a29e8619a4fa79a2326
Christopher Cole <christophercole1987@icloud.com>
2016-03-09 13:43:17 -0600

    Add Packaging Inv On-Hand tab to Portion Pac

--- js/pages/portion-pac.js
+++ js/pages/portion-pac.js
@@ -50,7 +50,7 @@ function packagingCheck(user_token, company_id) {
 	$.get("/src/packaging_inventory.php?act=list&user_token="+user_token+"&company_id"+company_id,function(result) {
 		var inventory = jQuery.parseJSON(result);
 		if(inventory.count > 0) {
-			$("#packagin_inventory_list").html('<table class="table sortable"><thead><tr><th class="width_100" data-defaultsort="asc">Class</th><th class="width_180">Item #</th><th>Product Name</th><th>Batch #</th><th class="width_100">Exp. Date</th><th class="text_right">Quantity</th><th>UOM</th><th>Location</th></tr></thead><tbody></tbody></table>');
+			$("#packaging_inventory_list").html('<table class="table sortable"><thead><tr><th class="width_100" data-defaultsort="asc">Class</th><th class="width_180">Item #</th><th>Product Name</th><th>Batch #</th><th class="width_100">Exp. Date</th><th class="text_right">Quantity</th><th>UOM</th><th>Location</th></tr></thead><tbody></tbody></table>');
 			jQuery.each( inventory.data, function( i, inv ) {
 				$("#packaging_inventory_list tbody").append('<tr><td class="width_100">'+inv.ItemGroupId+'</td><td class="width_180">'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td>'+inv.BatchNumber+'</td><td>'+inv.expDate+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td>'+inv.BOMUnitId+'</td><td>'+inv.Location+'</td></tr>');
 			});
