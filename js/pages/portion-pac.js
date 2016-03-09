fe49452d0906e13cb1fdbd3517f4be6f092b631a
Christopher Cole <christophercole1987@icloud.com>
2016-03-09 11:30:45 -0600

    Change header for Portion Pac On-Hand Inv

--- js/pages/portion-pac.js
+++ js/pages/portion-pac.js
@@ -1,6 +1,8 @@
 function inventoryCheck(user_token, company_id) {
 	$("#inventory_list").html('<div id="loading"><img src="images/spin.gif" /></div>');
 	$("#inventory_container").show();
+	$(".col-xs-10").html('');
+	$(".col-xs-10").html('<h1 class="page-header">FG Inv On-Hand</h1>');
 	$.get("/src/inventory.php?act=list&user_token="+user_token+"&company_id="+company_id,function(result) {
 		var inventory = jQuery.parseJSON(result);
 		if(inventory.count > 0) {
