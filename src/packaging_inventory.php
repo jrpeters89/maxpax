094b1086d94cdca49cda0f4db643776573d8deff
Christopher Cole <christophercole1987@icloud.com>
2016-03-10 11:04:14 -0600

    Filter packaging by group id and item

--- src/packaging_inventory.php
+++ src/packaging_inventory.php
@@ -33,7 +33,7 @@ if(!empty($user_token)) {
             foreach($data as $inv) {
                 if(substr($inv['ItemId'], 0, 3) == "PAC" && $inv['CompanyName'] == "MaxPax LLC" && ($inv['ItemGroupId'] == "LB" || $inv['ItemGroupId'] == "PM")) {
 
-                    $inventory['data'][$inv['ItemGroupId']][$inv['ItemId']]['ItemId'] = ($inv['ItemId'] != null ? $inv['ItemId'] : "");
+                    //$inventory['data'][$inv['ItemGroupId']][$inv['ItemId']]['ItemId'] = ($inv['ItemId'] != null ? $inv['ItemId'] : "");
 
                     $inventory['data'][$inv['ItemGroupId']][$inv['ItemId']]['AvailPhysical'] = ($inv['AvailPhysical'] != null) ? number_format($inv['AvailPhysical'],0,".",",") : "";
                     $inventory['data'][$inv['ItemGroupId']][$inv['ItemId']]['BatchNumber'] = ($inv['BatchNumber'] != null) ? $inv['BatchNumber'] : "";
