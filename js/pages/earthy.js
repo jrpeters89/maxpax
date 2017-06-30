d86b715b5d3f491ce122a7dc1d923d4ef2ff945e
Christopher Cole <christophercole1987@icloud.com>
2017-06-30 15:00:06 -0400

    Remove Open Sales tab from Earthy

--- js/pages/earthy.js
+++ js/pages/earthy.js
@@ -29,37 +29,6 @@ $("body").on("click",".opensales_tab",function(event) {
     opensalesCheck(user_token,type);
 });
 
-function opensalesCheck(user_token,type) {
-    type = (typeof type !== "undefined" ? type : "list");
-    $("#opensales_list").html('<div id="loading"><img src="images/spin.gif" /></div>');
-    $("#opensales_container").show();
-    $(".col-xs-10").html('');
-    $(".col-xs-10").html('<h1 class="page_header">Open Sales</h1>');
-    $.get("/src/open_sales.php?type="+type+"&user_token="+user_token,function(result) {
-        var sales = jQuery.parseJSON(result);
-        if(sales.count > 0) {
-            $("#opensales_list").html("");
-            if(type == "items") {
-                var count = 0;
-                jQuery.each( sales.data, function( i, sale ) {
-                    $("#opensales_list").append('<table id="item_'+count+'" class="table"><thead><tr><th>Item #</th><th colspan="7">Description</th></tr><tr><th>'+i+'</th><th colspan="7">'+sale.description+'</th></tr></thead><tbody><tr><td>Cust Ref</td><td>Sales Order</td><td>Unit</td><td class="text_right">Quantity</td><td class="text_right">Shipped</td><td class="text_right">Remainder</td><td>Delivery Address</td></tr></tbody></table>');
-                    jQuery.each( sale.data, function( s, item ) {
-                        $("#item_"+count+" tbody").append('<tr><td>'+item.CustomerRef+'</td><td>'+item.SalesId+'</td><td>'+item.SalesUnit+'</td><td class="text_right">'+item.SalesQty+'</td><td class="text_right">'+item.Shipped+'</td><td class="text_right">'+item.Remainder+'</td><td class="width_170">'+item.DeliveryAddress+'</td></tr>');
-                    });
-                    count++;
-                });
-            } else {
-                $("#opensales_list").html('<table class="table sortable"><thead><tr><th data-defaultsort="asc">Cust Ref</th><th class="width_120">Sales Order</th><th>Item #</th><th>Description</th><th>Unit</th><th class="text_right">Quantity</th><th class="text_right">Shipped</th><th class="text_right">Remainder</th></tr></thead><tbody></tbody></table>');
-                jQuery.each( sales.data, function( i, sale ) {
-                    $("#opensales_list tbody").append('<tr><td>'+sale.CustomerRef+'</td><td class="width_120">'+sale.SalesId+'</td><td>'+sale.ItemId+'</td><td>'+sale.ItemName+'</td><td>'+sale.SalesUnit+'</td><td class="text_right">'+sale.SalesQty+'</td><td class="text_right">'+sale.Shipped+'</td><td class="text_right">'+sale.Remainder+'</td></tr>');
-                });
-                $.bootstrapSortable(false);
-            }
-        } else {
-            $("#opensales_list").html("No Inventory Available");
-        }
-    });
-}
 
 function shippingData(user_token, company_id) {
     hold_company_id = company_id;
