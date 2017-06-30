181cfe6da73560b08967c1d318285597b8bdfd17
Christopher Cole <christophercole1987@icloud.com>
2017-06-30 12:53:12 -0400

    Updating On-Hand Inventory for Earthy and Treehouse

--- js/pages/treehouse.js
+++ js/pages/treehouse.js
@@ -1,23 +1,21 @@
 var hold_company_id;
 
 function inventoryCheck(user_token, company_id) {
+    hold_company_id = company_id;
     $("#inventory_list").html('<div id="loading"><img src="images/spin.gif" /></div>');
     $("#inventory_container").show();
     $(".col-xs-10").html('');
     $(".col-xs-10").html('<h1 class="page_header">On-Hand Inventory</h1>');
-    $.get("/src/inventory.php?act=list&user_token=" + user_token + "&company_id=" + company_id, function (result) {
+    $.get("/src/inventory.php?act=list&user_token="+user_token+"&company_id="+company_id,function(result) {
         var inventory = jQuery.parseJSON(result);
-        if (inventory.count > 0) {
-            $("#inventory_list").html('<table class="table sortable"><thead><tr><th>Location</th><th class="width_180">Item #</th><th>Product Name</th><th class="text_right">Quantity</th><th>UOM</th><th class="text_right">Case</th><th>Sell UOM</th><th class="text_right">Pallet</th><th class="width_180">Prod Date</th></tr></thead><tbody></tbody><tfoot></foot></table>');
-            jQuery.each(inventory.data, function (i, inv) {
-                $("#inventory_list tbody").append('<tr><td class="width_180">' + inv.Location + '</td><td class="width_120">' + inv.ItemId + '</td><td>' + inv.ItemName + '</td><td class="text_right">' + inv.AvailPhysical + '</td><td>' + inv.BOMUnitId + '</td><td class="text_right">' + inv.Case + '</td><td>' + inv.SellUOM + '</td><td class="text_right">' + inv.Pallet + '</td><td>' + inv.ProdDate + '</td></tr>');
+        if(inventory.count > 0) {
+            $("#inventory_list").html('<table class="table sortable"><thead><tr><th class="width_100" data-defaultsort="asc">Class</th><th class="width_180">Item #</th><th>Product Name</th><th>Batch #</th><th class="width_100">Exp. Date</th><th class="text_right">Quantity</th><th>UOM</th><th>Location</th></tr></thead><tbody></tbody></table>');
+            jQuery.each( inventory.data, function( i, inv ) {
+                $("#inventory_list tbody").append('<tr><td class="width_100">'+inv.ItemGroupId+'</td><td class="width_180">'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td>'+inv.BatchNumber+'</td><td>'+inv.expDate+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td>'+inv.BOMUnitId+'</td><td>'+inv.Location+'</td></tr>');
             });
-            if (typeof inventory.total != "undefined") {
-                $("#inventory_list tfoot").append('<tr><td colspan="2">TOTAL</td><td class="text_right">' + inventory.total.quantity + '</td><td></td><td class="text_right">' + inventory.total.case + '</td><td></td><td class="text_right">' + inventory.total.pallet + '</td></tr>');
-            }
             $.bootstrapSortable(false);
         } else {
-            $("#inventory_list").html("No On-Hand Inventory Available");
+            $("#inventory_list").html("No Inventory Available");
         }
     });
 }
