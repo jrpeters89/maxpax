efef8e323f6588d63eb4587211135efcb4a22cd1
Christopher Cole <christophercole1987@icloud.com>
2018-08-10 14:58:18 -0400

    Add COA to WF Young

--- src/documents.php
+++ src/documents.php
@@ -119,7 +119,27 @@ if(!empty($user_token)) {
 						$files['active'] = false;
 					}
 
-				} else {
+				} else if($row['id'] == 8) {
+                    $files['active'] = true;
+
+                    if (!empty($row['doc_path'])) {
+                        $dir = $row['doc_path'];
+
+                        $list = scandir($dir);
+
+                        foreach ($list as $key => $file) {
+                            $path = pathinfo($file);
+                            $files['list'][$key]['name'] = $path["filename"];
+                            if ($path["extension"] == "pdf") {
+                                $ext = "pdf";
+                            }
+                            $files['list'][$key]['ext'] = $ext;
+                            $files['list'][$key]['url'] = "/src/file.php?loc=" . $dir . "/" . $file . "&user_token=" . $user_token;
+                        }
+                    } else {
+                        $files['active'] = false;
+                    }
+                } else {
 					$files['active'] = true;
 
 					if (!empty($row['doc_path'])) {
