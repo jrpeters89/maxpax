60db630e9ae3368f3cba67b571b095591c9b3338
Christopher Cole <christophercole1987@icloud.com>
2018-12-03 13:51:15 -0500

    Adding Columns to the Inventory tab on the SW Fulfillment portal

--- js/pages/sw-fulfillment.js
+++ js/pages/sw-fulfillment.js
@@ -7,12 +7,12 @@ function inventoryCheck(user_token, company_id) {
     $("#inventory_container").show();
     $(".col-xs-10").html('');
     $(".col-xs-10").html('<h1 class="page_header">On-Hand Inventory</h1>');
-    $.get("/src/inventory.php?act=list&user_token="+user_token+"&company_id="+company_id,function(result) {
+    $.get("/src/inventory.php?act=list&user_token="+user_token+"&company_id="+hold_company_id+"&part_number="+partNumberTxt.value,function(result) {
         var inventory = jQuery.parseJSON(result);
         if(inventory.count > 0) {
-            $("#inventory_list").html('<table class="table sortable"><thead><tr><th class="width_180">Item #</th><th>Product Name</th><th class="text_right">On Order</th><th class="text_right">Available</th><th class="text_right">On Hand</th><th>UOM</th><th>Location</th></tr></thead><tbody></tbody></table>');
+            $("#inventory_list").html('<table class="table sortable"><thead><tr><th class="width_180">Item #</th><th>Product Name</th><th class="text_right">Quantity</th><th>UOM</th><th>Location</th></tr></thead><tbody></tbody></table>');
             jQuery.each( inventory.data, function( i, inv ) {
-                $("#inventory_list tbody").append('<tr><td class="width_180">'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td>'+inv.BOMUnitId+'</td><td>'+inv.Location+'</td></tr>');
+                $("#inventory_list tbody").append('<tr><td>'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td class="text_right">'+inv.OnOrder+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td class="text_right">'+inv.OnHand+'</td><td>'+inv.BOMUnitId+'</td><td>'+inv.Location+'</td></tr>');
             });
             $.bootstrapSortable(false);
         } else {
@@ -35,7 +35,7 @@ function refreshInventoryCheck(user_token) {
         if(inventory.count > 0) {
             $("#inventory_list").html('<table class="table sortable"><thead><tr><th class="width_180">Item #</th><th>Product Name</th><th class="text_right">Quantity</th><th>UOM</th><th>Location</th></tr></thead><tbody></tbody></table>');
             jQuery.each( inventory.data, function( i, inv ) {
-                $("#inventory_list tbody").append('<tr><td class="width_180">'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td class="text_right">'+inv.OnOrder+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td class="text_right">'+inv.OnHand+'</td><td>'+inv.BOMUnitId+'</td><td>'+inv.Location+'</td></tr>');
+                $("#inventory_list tbody").append('<tr><td>'+inv.ItemId+'</td><td>'+inv.ItemName+'</td><td class="text_right">'+inv.OnOrder+'</td><td class="text_right">'+inv.AvailPhysical+'</td><td class="text_right">'+inv.OnHand+'</td><td>'+inv.BOMUnitId+'</td><td>'+inv.Location+'</td></tr>');
             });
             $.bootstrapSortable(false);
         } else {
