ea132fb6d33b8a3aa1f3f4e53c98dce4938bca6d
Christopher Cole <christophercole1987@icloud.com>
2018-12-04 13:44:20 -0500

    Sort by purchase order and line number ascending

--- js/pages/sw-fulfillment.js
+++ js/pages/sw-fulfillment.js
@@ -52,7 +52,7 @@ function openPurchase(user_token, company_id) {
     $.get("/src/open_purchase.php?act=list&user_token=" + user_token + "&company_id=" + company_id, function (result) {
         var open_purchase = jQuery.parseJSON(result);
         if (open_purchase.count > 0) {
-            $("#open_purchase_list").html('<table class="table sortable"><thead><tr><th class="width_180" data-defaultsort="asc">Purchase Order</th><th class="width_100">Line #</th><th>Item Number</th><th>Description</th><th class="width_100">Delivery Date</th><th>Order Quantity</th><th>Deliver Remainder</th><th>Unit</th></tr></thead><tbody></tbody></table>');
+            $("#open_purchase_list").html('<table class="table sortable"><thead><tr><th class="width_180" data-defaultsort="desc">Purchase Order</th><th class="width_100">Line #</th><th>Item Number</th><th>Description</th><th class="width_100">Delivery Date</th><th>Order Quantity</th><th>Deliver Remainder</th><th>Unit</th></tr></thead><tbody></tbody></table>');
             jQuery.each(open_purchase.data, function (p, purch) {
                 $("#open_purchase_list tbody").append('<tr><td class="width_180">' + purch.PurchaseOrder + '</td><td class="width_100">' + purch.LineNumber + '</td><td>' + purch.ItemNumber + '</td><td>' + purch.Description + '</td><td>' + purch.DeliveryDate + '</td><td>' + purch.OrderQuantity + '</td><td>' + purch.DeliverRemainder + '</td><td>' + purch.Unit + '</td></tr>');
             });
