cf5cd24f7ecdf05cccaa76655c91a42a81110ecf
Christopher Cole <christophercole1987@icloud.com>
2019-01-15 13:31:24 -0500

    Updated document paths for COAs and Batch Tickets for Novis Works

--- src/documents.php
+++ src/documents.php
@@ -162,9 +162,9 @@ if(!empty($user_token)) {
 					$files['active'] = true;
 					if (!empty($row['doc_path'])) {
 						if ($tab == 'coas') {
-							$dir = $row['doc_path'] . '/Completed COAs 2018';
+							$dir = $row['doc_path'] . '/Completed COAs';
 						} else if ($tab == 'batch') {
-							$dir = $row['doc_path'] . '/FINISHED BATCH TICKETS 2018';
+							$dir = $row['doc_path'] . '/Completed Batch Tickets';
 						}
 						$list = scandir($dir);
 
